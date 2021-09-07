d5f3c4113c42f75882d96c373373513229083eec
Alex Merced <alex@alexmerced.dev>
2021-09-07 12:27:28 -0400

    sdfsdfsdf

--- todos/views.py
+++ todos/views.py
@@ -2,6 +2,8 @@ from .models import Todo
 from rest_framework import viewsets, permissions
 from .serializers import TodoSerializer
 
+cheese=5
+
 class TodoViewSet(viewsets.ModelViewSet):
     ## Give it the main query for the index route
     queryset = Todo.objects.all()
